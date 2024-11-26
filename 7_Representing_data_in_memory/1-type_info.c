27b78283a531623ae622b1fcb939edee58147dec
Krum Sultov <krsultov@gmail.com>
2024-11-26 14:01:27 +0200

    7 - Representing data in memory

---
+++ 7_Representing_data_in_memory/1-type_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define PRINT_TYPE_INFO(NAME, SIGNED_MIN, SIGNED_MAX, UNSIGNED_MAX) \
+
+
+int main() {
+
+    // print the size and info for all the integer types
+
+    return 0;
+}
