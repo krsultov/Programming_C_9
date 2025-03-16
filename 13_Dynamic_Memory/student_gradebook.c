1034c4e2bc359e77a91a11b8b6c72d8cc5d7b76f
krsultov <krsultov@gmail.com>
2025-03-16 18:39:35 +0200

    13 Dynamic Memory

--- 13_Dynamic_Memory/student_gradebook.c
+++ 13_Dynamic_Memory/student_gradebook.c
@@ -1,64 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * read_grades(int num_grades) {
-    int *grades = malloc(num_grades * sizeof(int));
-    if (grades == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
+#define CHECK_ALLOC(ptr) if ((ptr) == NULL) { \
+    printf("Memory allocation failed\n"); \
+    exit(1); \
+}
+
+float * read_grades(int num_grades) {
+    float *grades = malloc(num_grades * sizeof(float));
+    CHECK_ALLOC(grades);
 
     for (int i = 0; i < num_grades; i++) {
         printf("Enter grade %d: ", i + 1);
-        scanf("%d", &grades[i]);
+        scanf("%f", &grades[i]);
     }
     return grades;
 }
 
-float calculate_average(const int *grades, int num_grades) {
+float calculate_average(const float *grades, int num_grades) {
     float sum = 0;
     for (int i = 0; i < num_grades; i++) {
-        sum += (float)grades[i];
+        sum += grades[i];
     }
     return sum / (float)num_grades;
 }
 
-int * add_grade(int *grades, int *num_grades, int new_grade) {
-    int *new_grades = realloc(grades, (*num_grades + 1) * sizeof(int));
-    if (new_grades == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
+float * add_grade(float *grades, int *num_grades, float new_grade) {
+    float *new_grades = realloc(grades, (*num_grades + 1) * sizeof(float));
+    CHECK_ALLOC(new_grades);
 
     new_grades[*num_grades] = new_grade;
     (*num_grades)++;
     return new_grades;
 }
 
-int * delete_last_grade(int *grades, int *num_grades) {
-    int *new_grades = realloc(grades, (*num_grades - 1) * sizeof(int));
-    if (new_grades == NULL) {
-        printf("Memory allocation failed\n");
-        exit(1);
+float * delete_last_grade(float *grades, int *num_grades) {
+    if (*num_grades == 0) {
+        printf("No grades to delete\n");
+        return grades;
     }
+
+    if (*num_grades == 1) {
+        free(grades);
+        (*num_grades)--;
+        return NULL;
+    }
+
+    float *new_grades = realloc(grades, (*num_grades - 1) * sizeof(float));
+    CHECK_ALLOC(new_grades);
 
     (*num_grades)--;
     return new_grades;
 }
 
-void print_grades(int *grades, int num_grades) {
+void print_grades(float *grades, int num_grades) {
     printf("Grades: \n");
     for (int i = 0; i < num_grades; i++) {
-        printf("%d ", grades[i]);
+        printf("%.2f ", grades[i]);
     }
     printf("\n");
 }
 
 int main(void) {
-    printf("Enter the number of grades: ");
     int num_grades;
-    scanf("%d", &num_grades);
-    int* grades = read_grades(num_grades);
+    do {
+        printf("Enter the number of grades: ");
+        scanf("%d", &num_grades);
+        if (num_grades < 0) {
+            printf("Number of grades cannot be negative. Please enter a valid number.\n");
+        }
+    } while (num_grades < 0);
+
+    float* grades = read_grades(num_grades);
     print_grades(grades, num_grades);
 
     while (1) {
@@ -73,9 +86,9 @@ int main(void) {
         scanf("%d", &choice);
         switch (choice) {
             case 1: {
-                int new_grade;
+                float new_grade;
                 printf("Enter the new grade: ");
-                scanf("%d", &new_grade);
+                scanf("%f", &new_grade);
                 grades = add_grade(grades, &num_grades, new_grade);
                 print_grades(grades, num_grades);
                 break;
@@ -86,12 +99,16 @@ int main(void) {
                 break;
             }
             case 3: {
-                printf("Average: %.00f\n", calculate_average(grades, num_grades));
+                if (num_grades == 0) {
+                    printf("No grades available to calculate average.\n");
+                } else {
+                    printf("Average: %.2f\n", calculate_average(grades, num_grades));
+                }
                 break;
             }
             case 4: {
                 free(grades);
-                return 0;
+                exit(0);
             }
             default: {
                 printf("Invalid option\n");
@@ -99,5 +116,4 @@ int main(void) {
             }
         }
     }
-
 }
